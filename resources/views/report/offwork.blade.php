a2aa920c344bc8ec8ffe06c6b024d1d8d4ede895
Devvara Rishivian <qaisha.muhammada@edi-indonesia.co.id>
2021-07-01 11:03:49 +0700

    feat: list offwork

--- resources/views/report/offwork.blade.php
+++ resources/views/report/offwork.blade.php
@@ -20,14 +20,14 @@
                         <div class="row">
                             <div class="col-xs-3">
                                 <label>Tahun</label>
-                                <select class="form-control select2" style="width: 100%;" id="filterOffWorkYear" name="filterOffWorkYear">
+                                <select class="form-control select2" style="width: 100%;" id="filterYear" name="filterYear">
                                     <option value="">--Pilih Tahun--</option>
                                     @for ($i = 1995; $i <= date('Y'); $i++) <option value="{{ $i }}">{{ $i }}</option> @endfor
                                 </select>
                             </div>
                             <div class="col-xs-3">
                                 <label>Bulan</label>
-                                <select class="form-control select2" style="width: 100%;" id="filterOffWorkMonth" name="filterOffWorkMonth">
+                                <select class="form-control select2" style="width: 100%;" id="filterMonth" name="filterMonth">
                                     <option value="">--Pilih Bulan--</option>
                                     @foreach($months as $k => $v)
                                     <option value="{{ $k }}">{{ $v }}</option>
@@ -52,7 +52,124 @@
                 </div>
             </div>
         </div>
+
+        <div id="offworkTable"></div>
     </section>
-    <!-- /.content -->
 </div>
+
+<script type="module">
+    import {
+        Grid,
+        html
+    } from "https://unpkg.com/gridjs?module";
+
+
+    (function() {
+        // document ready
+    })();
+
+    let urlData = '<?= url('report/offwork/data') ?>';
+    let urlExport = '<?= url('report/offwork/export') ?>';
+    let urlFilter = '?';
+
+    let filterYear = document.getElementById('filterYear');
+    let filterMonth = document.getElementById('filterMonth');
+
+    let grid = new Grid({
+        language: {
+            'search': {
+                'placeholder': 'Ketik disini untuk mencari ...'
+            },
+            'pagination': {
+                'previous': 'Sebelumnya',
+                'next': 'Selanjutnya',
+                'showing': 'Menampilkan',
+                of: 'dari',
+                to: 'sampai',
+                'results': () => 'Pegawai'
+            }
+        },
+        fixedHeader: true,
+        height: '500px',
+        search: true,
+        sort: true,
+        columns: [
+            "NIK",
+            "Nama",
+            "Awal Cuti",
+            "Akhir Cuti",
+            "Jumlah Hari",
+            "Alamat Cuti",
+            "Status",
+        ],
+        server: {
+            url: urlData,
+            then: data => data.map(val => [
+                val.NIK,
+                val.NAMA,
+                val.AWAL_CUTI,
+                val.AKHIR_CUTI,
+                val.JUMLAH_CUTI,
+                val.ALAMAT_CUTI,
+                val.STATUS,
+            ])
+        },
+        pagination: {
+            enabled: true,
+            limit: 10,
+            summary: true
+        }
+    }).render(document.getElementById("offworkTable"));
+
+    window.applyFilter = () => {
+        let urlQ1 = `filterYear=${filterYear.value}`;
+        let urlQ2 = `&filterMonth=${filterMonth.value}`;
+
+        urlFilter = `?${urlQ1}${urlQ2}`;
+        urlData = `<?= url('report/offwork/data') ?>${urlFilter}`
+        urlExport = `<?= url('report/offwork/export') ?>${urlFilter}`
+
+        grid.updateConfig({
+            server: {
+                url: urlData,
+                then: data => data.map(val => [
+                    val.NIK,
+                    val.NAMA,
+                    val.AWAL_CUTI,
+                    val.AKHIR_CUTI,
+                    val.JUMLAH_CUTI,
+                    val.ALAMAT_CUTI,
+                    val.STATUS,
+                ])
+            },
+        }).forceRender();
+    }
+
+    window.resetFilter = () => {
+        filterYear.value = '';
+        filterMonth.value = '';
+
+        urlData = '<?= url('report/offwork/data') ?>';
+        urlExport = '<?= url('report/offwork/export') ?>';
+
+        grid.updateConfig({
+            server: {
+                url: urlData,
+                then: data => data.map(val => [
+                    val.NIK,
+                    val.NAMA,
+                    val.AWAL_CUTI,
+                    val.AKHIR_CUTI,
+                    val.JUMLAH_CUTI,
+                    val.ALAMAT_CUTI,
+                    val.STATUS,
+                ])
+            },
+        }).forceRender();
+    }
+
+    window.exportExcel = () => {
+        window.open(urlExport, '_blank');
+    }
+</script>
 @endsection
