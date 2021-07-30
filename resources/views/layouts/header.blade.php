68d09e33596f234577ce7e26cbf4af79e0d6144a
Devvara Rishivian <qaisha.muhammada@edi-indonesia.co.id>
2021-07-30 18:24:10 +0700

    feat: change header logo text

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,7 @@
     <!-- mini logo for sidebar mini 50x50 pixels -->
     <span class="logo-mini"><b>HR</b>IS</span>
     <!-- logo for regular state and mobile devices -->
-    <span class="logo-lg"><b>EDII.</b>HRIS</span>
+    <span class="logo-lg"><b>Hadirkoe</b> HRIS</span>
   </a>
   <!-- Header Navbar: style can be found in header.less -->
   <nav class="navbar navbar-static-top">
